125bd891ddf88b39e56beb73550c35d14fc1f817
Melissa Wei <melissa.weiibm.com@melissas-mbp-2.raleigh.ibm.com>
2018-12-04 16:52:49 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3001;
+
 //creating the app
 var app = express();
 
@@ -62,5 +64,6 @@ app.get('/bad', (req, res) => {
 });
 
 //bind application to a local port
-app.listen(3001);
-  console.log('Server is up on port 3001');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
