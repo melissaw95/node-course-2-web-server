8d9f6e397211c40fea31d197d5ee1a446292088e
Melissa Wei <melissa.weiibm.com@melissas-mbp-2.raleigh.ibm.com>
2018-12-06 10:07:47 -0500

    updated projects file name

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get ('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('project.hbs', {
+  res.render('projects.hbs', {
     pageTitle: 'Projects Page',
     welcomeMessage: 'Projects:'
   });
